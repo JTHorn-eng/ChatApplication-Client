080768c15dff1e0fabb25c9f59eeb0acbf707195
JTHorn-eng <57071303+JTHorn-eng@users.noreply.github.com>
2020-07-20 10:00:28 +0100

    Add files via upload

---
+++ ClientApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Chat.client
+{
+    class ClientApplication
+    {
+
+
+        private Client client;
+        public ClientApplication()
+        {
+            //connect to server
+            //download messages for user
+            //decrypt messages using private key
+            //add messages to database
+            //server deletes downloaded messages
+            //populate friends UI scrollviewer with friends list from database
+
+
+            //WHILE APPLICATION IS RUNNING
+            //Stay connected to server so it can listen to client + friends events
+            Console.WriteLine("Hello");
+            client = new Client();
+            
+        
+        
+        }
+
+
+
+    }
+}

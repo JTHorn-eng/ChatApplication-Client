8849e6d2bac9ec70c1760ccf60ae57dcfb144817
JTHorn-eng <horn19556@gmail.com>
2020-07-20 13:51:24 +0100

    Added new files

---
+++ ChatApplication-Client/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Chat.client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication_Client
+{
+    class  MainWindow 
+    {
+
+        private ClientApplication client;
+
+        public MainWindow()
+        {
+            client = new ClientApplication();
+        }
+
+        public static void Main(String[] args)
+        {
+            MainWindow mw = new MainWindow();
+        }
+
+    }
+}

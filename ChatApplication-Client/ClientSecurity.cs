2874f10db49336fc192b4dd1ebf602cdb2ce609a
JTHorn-eng <horn19556@gmail.com>
2020-08-08 20:42:41 +0100

    Security code overhaul

--- ChatApplication-Client/ClientSecurity.cs
+++ ChatApplication-Client/ClientSecurity.cs
@@ -3,208 +3,170 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace ChatClient
+namespace ChatServer
 {
     public static class ClientSecurity
     {
+        //store encrypted symmetric keys
+        static byte[] encryptedSymmetricKey;
+        static byte[] encryptedSymmetricIV;
+        static byte[] SymmetricKey;
+        static byte[] SymmetricIV;
+        static byte[] generatedCipherText;
 
-        private static byte[] publicKey;
-        private static byte[] IV;
-        private static RSAParameters RSAKeyInfo;
+        static RSAParameters RSAPublicKey;
+        static RSAParameters RSAPrivateKey;
 
-        //Generates a new symmetric key and stores it in key container. If the key already
-        //exists, then a new key is generated. Returns public-private key pair
-   
-        public static string GenKey(string containerName)
+        public static byte[] Encrypt(string plaintext)
         {
-            //Generate a new key container.
-            CspParameters p = new CspParameters();
-            p.KeyContainerName = containerName;
-
-            //Generate public, private key pair
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(p);
-            rsa.PersistKeyInCsp = false;
-
-            //Overwrite key pair if same containerName is specified
-            rsa.Clear();
-            CspParameters p2 = new CspParameters();
-            p.KeyContainerName = containerName;
-
-            RSACryptoServiceProvider rsa2 = new RSACryptoServiceProvider(p2);
-            return rsa2.ToXmlString(true);
+            generatedCipherText = AESEncrypt(plaintext);
+            EncryptPublicKeyAndIV();
+            return generatedCipherText;
+        }
+        public static string Decrypt(byte[] cipherText)
+        {
+            DecryptPublicKeyAndIV(encryptedSymmetricKey, encryptedSymmetricIV);
+            return AESDecrypt(cipherText);
         }
 
-        //Retrieve key pair with specified key container name. Returns public-private key pair
-        public static string RetrieveKeyPair(string containerName)
+        private static void EncryptPublicKeyAndIV()
         {
-            CspParameters p = new CspParameters();
-            p.KeyContainerName = containerName;
 
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(p);
-            return rsa.ToXmlString(true);
-        }
+            //Create a new instance of RSACryptoServiceProvider.
 
-        //Only retrieve RSA public key as a string literal
-        public static string RetrievePublicKey(string containerName)
-        {
-            CspParameters p = new CspParameters();
-            p.KeyContainerName = containerName;
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
 
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(p);
-            return rsa.ToXmlString(false);
 
+            //Import key parameters into RSA
+            RSA.ImportParameters(RSAPublicKey);
+
+            //Encrypt AES public key and IV
+            encryptedSymmetricKey = RSA.Encrypt(SymmetricKey, false);
+            encryptedSymmetricIV = RSA.Encrypt(SymmetricIV, false);
 
         }
-
-        //Retrieve RSA public key only as an RSAParameter object
-        private static RSAParameters GetPublicKeyInfo(string containerName)
+        private static void DecryptPublicKeyAndIV(byte[] PublicKey, byte[] IV)
         {
-            CspParameters p = new CspParameters();
-            p.KeyContainerName = containerName;
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
 
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(p);
-            return rsa.ExportParameters(false);
-            
+            //Import key parameters into RSA
+            RSA.ImportParameters(RSAPrivateKey);
+
+            SymmetricKey = RSA.Decrypt(PublicKey, false);
+            SymmetricIV = RSA.Decrypt(IV, false);
         }
 
-        //Retrieve RSA key pair as an RSAParameter object
-        private static RSAParameters GetKeyPairInfo(string containerName)
+        public static RSAParameters GetRSAPublicKey(string container)
         {
+            //Create crypto service provider parameters
             CspParameters p = new CspParameters();
-            p.KeyContainerName = containerName;
+            p.KeyContainerName = container;
 
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(p);
-            return rsa.ExportParameters(true);
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(p);
+            RSA.PersistKeyInCsp = false;
+
+
+            return RSA.ExportParameters(false);
+        }
+
+        private static RSAParameters GetRSAPrivateKey(string container)
+        {
+            //Create crypto service provider parameters
+            CspParameters p = new CspParameters();
+            p.KeyContainerName = container;
+
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(p);
+            RSA.PersistKeyInCsp = false;
+
+
+            return RSA.ExportParameters(true);
+        }
+
+
+        //Establish AES public key and IV
+        public static void Init(string keyContainer)
+        {
+            Aes aes = Aes.Create();
+            RSAPublicKey = GetRSAPublicKey(keyContainer);
+            RSAPrivateKey = GetRSAPrivateKey(keyContainer);
+            aes.GenerateKey();
+            aes.GenerateIV();
+            SymmetricKey = aes.Key;
+            SymmetricIV = aes.IV;
 
         }
 
-        //Encrypt plaintext using AES algorithm. Returns the ciphertext
-        private static byte[] EncryptStringToBytes_Aes(string plainText)
+        private static byte[] AESEncrypt(string plainText)
         {
-            if (plainText.Length >= 0)
+            byte[] encrypted;
+
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
             {
-                //create an AES object, key and IV
-                Aes aesAlg = Aes.Create();
-             
 
-                //create an ecryptor to perform the stream transform.
+
+                aesAlg.Key = SymmetricKey;
+                aesAlg.IV = SymmetricIV;
+
+                // Create an encryptor to perform the stream transform.
                 ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
-                //Create the byte streams used for encryption
-                MemoryStream msEncrypt = new MemoryStream();
-                CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
-                StreamWriter swEncrypt = new StreamWriter(csEncrypt);
-                swEncrypt.Write(plainText);
-                publicKey = aesAlg.Key;
-                IV = aesAlg.IV;
-                return msEncrypt.ToArray();
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
             }
-            else
-            {
-                Console.WriteLine("Could not encrypt plainText");
-                return new byte[0];
-            }
+            return encrypted;
         }
 
-        //Generates fully encrypted message
-        //Currently uses client username as containerName
-        //OUTPUT-FORMAT: cipher-text|encrypted-AES-public-key|encrypted-IV
-        public static string EncryptMessage(string plainText, string containerName)
+        private static string AESDecrypt(byte[] cipherText)
         {
+            string plaintext = "";
+            // Create an AesCryptoServiceProvider object
+            // with the specified key and IV.
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = SymmetricKey;
+                aesAlg.IV = SymmetricIV;
 
-            return Encoding.UTF8.GetString(EncryptStringToBytes_Aes(plainText)) + "|"
-                  + Encoding.UTF8.GetString(RSAEncryption(publicKey, GetPublicKeyInfo(containerName), false)) + "|"
-                  + Encoding.UTF8.GetString(RSAEncryption(IV, GetPublicKeyInfo(containerName), false));
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
         }
-
-        //Decrypt fully encrypted message
-        //Currently uses client username as containerName
-        //OUTPUT-FORMAT: plain-text
-        public static string DecryptMessage(string inputText, string containerName)
+        //FOR DEBUGGING ONLY
+        public static void Test(string containerName)
         {
-            //Decrypt AES public key and IV
-            byte[] publicKey = Encoding.UTF8.GetBytes(inputText.Split('|')[1]);
-            byte[] IV = Encoding.UTF8.GetBytes(inputText.Split('|')[2]);
-            publicKey = RSADecryption(publicKey, GetKeyPairInfo(containerName), false);
-            IV = RSADecryption(IV, GetKeyPairInfo(containerName), false);
-
-            //return AES decrypted cipher text
-            return DecryptBytesToString_Aes(Encoding.UTF8.GetBytes(inputText), publicKey, IV);
+            string message = "Hello Testing Testing 1 2 3";
+            Console.WriteLine(message);
+            Console.WriteLine(Encrypt(message));
+            Console.WriteLine(Decrypt(Encrypt(message)));
         }
 
-
-
-        //Decrypt ciphertext using AES algorithm. Returns the plaintext
-        private static string DecryptBytesToString_Aes(byte[] cipherText, byte[] key, byte[] IV)
-        {
-            string plainText = null;
-            if (cipherText.Length <= 0 || key.Length <= 0 || IV.Length <= 0)
-            {
-                Aes aesAlg = Aes.Create();
-                aesAlg.Key = key;
-                aesAlg.IV = IV;
-
-                //create an ecryptor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                //Create the byte streams used for encryption
-                MemoryStream msDecrypt = new MemoryStream(cipherText);
-                CryptoStream cdDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
-                StreamReader srDecrypt = new StreamReader(cdDecrypt);
-                plainText = srDecrypt.ReadToEnd();
-
-                return plainText;
-            }
-            else
-            {
-                Console.WriteLine("[INFO] Could not decrypt cipherText");
-                return "";
-            }
-        }
-
-        //Encrypt data using RSA algorithm. Returns the encrypted data
-        public static byte[] RSAEncryption(byte[] Data, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] encryptedData;
-                RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
-                
-                RSA.ImportParameters(RSAKeyInfo);
-                encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
-                return encryptedData;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine("Error encrypting message: " + e.Message);
-            }
-            return null;
-        }
-
-        //Decrypt data using RSA algorithm. Returns the decrypted data
-        private static byte[] RSADecryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
-        {
-            try
-            {
-                byte[] decryptedData;
-                RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
-                RSA.ImportParameters(RSAKey);
-                decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
-                return decryptedData;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine("Error encrypting message: " + e.Message);
-            }
-            return null;
-        }
-
-        //generate a digital signature for a message using the plaintext
-        public static byte[] GenerateDigitalSignature(string plainText)
-        {
-            //hash message
-            HashAlgorithm sha = new SHA1CryptoServiceProvider();
-            return sha.ComputeHash(Encoding.UTF8.GetBytes(plainText));
-        }
     }
 }
+
